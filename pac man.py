9de9e1a97e9619b8ee79b4d81571263162a1b628
Dedeil <40626932+Dedeil@users.noreply.github.com>
2021-04-09 15:17:56 +0200

    Add files via upload

--- pac man.py
+++ pac man.py
@@ -10,27 +10,28 @@ class jeu():
         self.__y=7*self.__cote
         self.__w=19
         self.__h=21
-        self.__speed=9
+        self.__vitesse=9
         self.__f=Tk()
         self.__f.title("Puck-Man.pptx")
         self.__carte=Canvas(self.__f,height=self.__cote*self.__h,width=self.__cote*self.__w)
 
         self.__coordMurs=[]
         self.__coordBonus=[]
+        self.__bonus=[]
         
-        self.createMap()
+        self.creerCarte()
 
         self.__pacman=self.__carte.create_oval(self.__x+1,self.__y+1,self.__x+self.__cote-1,self.__y+self.__cote-1,fill="yellow")
         
-        self.__f.bind("<z>",self.__north)
-        self.__f.bind("<q>",self.__west)
-        self.__f.bind("<s>",self.__south)
-        self.__f.bind("<d>",self.__east)
+        self.__f.bind("<z>",self.__nord)
+        self.__f.bind("<q>",self.__ouest)
+        self.__f.bind("<s>",self.__sud)
+        self.__f.bind("<d>",self.__est)
         
         self.__carte.pack()
         self.__f.mainloop()
     
-    def createMap(self):
+    def creerCarte(self):
         f = open("map2.txt", "r")
         x,y=0,0
         for ligne in f:
@@ -44,63 +45,74 @@ class jeu():
                 x+=1
             y+=1
             x=0
-        print(self.__coordMurs)
-        self.addBonus()
+        #print(self.__coordMurs)
+        self.ajouterBonus()
     
-    def addBonus(self):
+    def ajouterBonus(self):
         f=open("map2_bonus.txt", "r")
         x,y=0,0
         for ligne in f:
             ligne=ligne[:-1].split(",")
             for chiffre in ligne:
                 if chiffre=="1":
-                    self.__carte.create_oval(x*self.__cote+9,y*self.__cote+9,(1+x)*self.__cote-9,(1+y)*self.__cote-9,fill="white")
-                    self.__coordBonus.append((x*self.__cote+9,y*self.__cote+9))
+                    self.__bonus.append(self.__carte.create_oval(x*self.__cote+9,y*self.__cote+9,(1+x)*self.__cote-9,(1+y)*self.__cote-9,fill="white"))
+                    self.__coordBonus.append(((x+.5)*self.__cote,(y+.5)*self.__cote))
                 x+=1
             y+=1
             x=0
-            print(self.__coordBonus)
+            #print(self.__coordBonus)
 
-    def __north(self,poop):
-        yTmp=self.__y-self.__speed
+    def __nord(self,poop):
+        yTmp=self.__y-self.__vitesse
         for i in self.__coordMurs:
             if i[0]<=self.__x and self.__x+self.__cote<=i[0]+self.__cote and i[1]<=yTmp<i[1]+self.__cote:
-                self.__y-=self.__speed
+                self.__y-=self.__vitesse
                 self.__carte.coords(self.__pacman,self.__x+1,self.__y+1,self.__x+self.__cote-1,self.__y+self.__cote-1)
+                self.__supprBonus(i)
                 break
 
-    def __west(self,poop):
-        xTmp=self.__x-self.__speed
+    def __ouest(self,poop):
+        xTmp=self.__x-self.__vitesse
         for i in self.__coordMurs:
             if xTmp<0:
                 self.__x+=self.__cote*(self.__w-1)
                 self.__carte.coords(self.__pacman,self.__x+1,self.__y+1,self.__x+self.__cote-1,self.__y+self.__cote-1)
+                self.__supprBonus(i)
                 break
             elif i[0]<=xTmp<i[0]+self.__cote and i[1]<=self.__y and self.__y+self.__cote<=i[1]+self.__cote:
-                self.__x-=self.__speed
+                self.__x-=self.__vitesse
                 self.__carte.coords(self.__pacman,self.__x+1,self.__y+1,self.__x+self.__cote-1,self.__y+self.__cote-1)
-#                for coord in self.__coordBonus:
-#                    if self.__x
                 break
 
-    def __south(self,poop):
-        yTmp=self.__y+self.__speed
+    def __sud(self,poop):
+        yTmp=self.__y+self.__vitesse
         for i in self.__coordMurs:
             if i[0]<=self.__x and self.__x+self.__cote<=i[0]+self.__cote and i[1]-self.__cote<=yTmp<=i[1]:
-                self.__y+=self.__speed
+                self.__y+=self.__vitesse
                 self.__carte.coords(self.__pacman,self.__x+1,self.__y+1,self.__x+self.__cote-1,self.__y+self.__cote-1)
+                self.__supprBonus(i)
                 break
 
-    def __east(self,poop):
-        xTmp=self.__x+self.__speed+self.__cote
+    def __est(self,poop):
+        xTmp=self.__x+self.__vitesse+self.__cote
         for i in self.__coordMurs:
-            if xTmp>self.__w*self.__cote:
+            if i[0]<xTmp<=i[0]+self.__cote and i[1]<=self.__y and self.__y+self.__cote<=i[1]+self.__cote:
+                self.__x+=self.__vitesse
+                self.__carte.coords(self.__pacman,self.__x+1,self.__y+1,self.__x+self.__cote-1,self.__y+self.__cote-1)
+                self.__supprBonus(i)
+                break
+            elif xTmp>self.__w*self.__cote:
                 self.__x=0
                 self.__carte.coords(self.__pacman,self.__x+1,self.__y+1,self.__x+self.__cote-1,self.__y+self.__cote-1)
                 break
-            if i[0]<xTmp<=i[0]+self.__cote and i[1]<=self.__y and self.__y+self.__cote<=i[1]+self.__cote:
-                self.__x+=self.__speed
-                self.__carte.coords(self.__pacman,self.__x+1,self.__y+1,self.__x+self.__cote-1,self.__y+self.__cote-1)
+
+    def __supprBonus(self,i):
+        for j in self.__coordBonus:
+            if abs(i[0]+self.__cote*.5-j[0])<=9 and abs(i[1]+self.__cote*.5-j[1])<=9:
+                indice=self.__coordBonus.index(j)
+                self.__coordBonus.pop(indice)
+                self.__carte.coords(self.__bonus[indice],0,0,0,0)
+                self.__bonus.pop(indice)
                 break
         
 main=jeu()
